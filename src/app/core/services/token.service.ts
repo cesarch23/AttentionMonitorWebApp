49c5976b071d12dd3684225adc7967e935f52364
Cesar Choccata <c21choccata@gmail.com>
2025-06-04 01:38:29 -0500

    Add TokenService for managing authentication tokens

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { setCookie,getCookie,removeCookie } from 'typescript-cookie'
+import { jwtDecode, JwtPayload} from 'jwt-decode'
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+  saveToken(token:string){
+    setCookie('token',token,{expires:365,path:'/'})
+  }
+  getToken(){
+    getCookie('token')
+  }
+  removeToken(){
+    removeCookie('token')
+  }
+  isValidToken():boolean{
+    const token = getCookie('token');
+    if(!token) return false;
+    const decodeToken = jwtDecode<JwtPayload>(token);
+    console.log(" token decodificado ",decodeToken);
+    if(decodeToken && decodeToken.exp){
+      const today = new Date();
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodeToken.exp)
+      return today.getTime() < tokenDate.getTime();
+    }
+    return false;
+
+  }
+}
