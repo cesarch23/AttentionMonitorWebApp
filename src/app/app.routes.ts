073ec82950288f64d3811e8098327f0ed73f5da7
Cesar Choccata <c21choccata@gmail.com>
2025-06-06 14:18:44 -0500

    Refactor route definitions to include role-based access control for 'estudiante' and 'admin' paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { roleGuard } from './core/guards/role.guard'
 
 export const routes: Routes = [
     {
@@ -6,12 +8,20 @@ export const routes: Routes = [
         loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
     },
     {
-        path: 'student',
-        loadChildren: () => import('./student/student.routes').then(m => m.STUDENT_ROUTES)
+        path: 'estudiante',
+        canActivate: [authGuard, roleGuard],
+        loadChildren: () => import('./student/student.routes').then(m => m.STUDENT_ROUTES),
+        data: {
+            roles: ['ESTUDIANTE']
+        }
     },
     {
+        canActivate: [authGuard,roleGuard],
         path: 'admin',
-        loadChildren: () => import('./teacher/teacher.routes').then(m => m.TEACHER_ROUTES)
+        loadChildren: () => import('./teacher/teacher.routes').then(m => m.TEACHER_ROUTES),
+        data: {
+            roles: ['PROFESOR']
+        }
     },
     {
         path: '**',
