623d363a42bcaf3149205056f8aa6d98d90bc734
Cesar Choccata <c21choccata@gmail.com>
2025-06-06 01:16:41 -0500

    Add NotificationService for displaying customizable snack bar notifications

---
+++ src/app/core/services/notification.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotificationComponent } from '../../shared/components/notification/notification.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  private matSnackBar = inject(MatSnackBar) 
+  constructor() { }
+
+   show(message: string, type: 'success' | 'error' | 'info' | 'warning' = 'info', durationSeg = 3) {
+    let icon: 'info' | 'warning' | 'error' | 'check' = 'info';
+    
+    if(type === 'success') icon = 'check';
+    if(type === 'error') icon = 'error';
+    if(type === 'info') icon = 'info';
+    if(type === 'warning') icon = 'warning';
+
+    this.matSnackBar.openFromComponent(NotificationComponent, {
+      data: { message, type, icon },
+      duration: durationSeg*1000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['custom-snackbar-panel']
+    });
+  }
+}
