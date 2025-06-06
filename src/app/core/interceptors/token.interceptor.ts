32ddba9b1ee7be009010050bdfd5bdda7fab0ee6
Cesar Choccata <c21choccata@gmail.com>
2025-06-06 13:02:13 -0500

    Refactor token interceptor to improve context handling and error management

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,23 +1,29 @@
-import { HttpContextToken, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { HttpContext, HttpContextToken, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { TokenService } from '../services/token.service';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { of } from 'rxjs';
+import { catchError, of, tap, throwError } from 'rxjs';
 
 export const CACHING_ENABLED = new HttpContextToken<boolean>(()=>true);
+export const EXPECT_TOKEN_IN_RESPONSE = new HttpContextToken<boolean>(() => false);
 
 export function setCachingEnabled(){
-  return new HttpContextToken<boolean>(()=>false)
+  return new HttpContext().set(CACHING_ENABLED, false);
 }
-
+export function expectToken() {
+  return new HttpContext().set(EXPECT_TOKEN_IN_RESPONSE, true);
+}
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
   const authService = inject(AuthService);
+  const shouldExpectToken = req.context.get(EXPECT_TOKEN_IN_RESPONSE);
 
   if(!req.context.get(CACHING_ENABLED)){
     const isValidToken = tokenService.isValidToken();
+    console.log("isValidToken", isValidToken);
     if(isValidToken){
       const token = tokenService.getToken();
+      console.log("token interceptor", token);
       if(token){
         const newRequest = req.clone({
           headers: req.headers.set('Authorization', `Bearer ${token}`)
@@ -26,13 +32,25 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
       }
       else{
         authService.logout();
-        // Return an unauthorized response instead of the request
         return of(new HttpResponse({
           status: 401,
           statusText: 'Unauthorized'
         }));
       }
-    } 
+    }
+  
   }
-  return next(req);
+  return next(req).pipe(
+    tap(event => {
+      if (shouldExpectToken && event instanceof HttpResponse) {
+        const token = event.headers.get('Authorization');
+        if (!token) {
+          throw new Error('Token ausente en la respuesta');
+        }
+      }
+    }),
+    catchError(err => {
+      return throwError(() => err);
+    })
+  );
 };
