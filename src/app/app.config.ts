b70189bbee100124ab5a34722cc8b515b5b3c6f2
Cesar Choccata <c21choccata@gmail.com>
2025-06-06 01:17:01 -0500

    Configure HTTP client with token interceptor for authorization handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideAnimationsAsync(),
-    //provideHttpClient()
+    provideHttpClient(
+      withInterceptors([tokenInterceptor])
+    )
   ]
 };
