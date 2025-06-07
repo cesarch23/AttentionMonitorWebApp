53affd0487db8ede28a5d57949953bfb532991c8
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:21:02 -0500

    Refactor AuthService to include profile retrieval for students and teachers, and update registration endpoint

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient, HttpContext, HttpErrorResponse, HttpResponse, HttpStatusCode } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { enviroment } from '../../../environments/environment';
 import { catchError, switchMap, tap, throwError } from 'rxjs';
 import { TokenService } from './token.service';
-import { UserRegister } from '../models/model.interface';
+import { StudentProfile, TeacherProfile, UserRegister } from '../models/model.interface';
 import { Router } from '@angular/router';
-import { CACHING_ENABLED, EXPECT_TOKEN_IN_RESPONSE, setCachingEnabled } from '../interceptors/token.interceptor';
+import { EXPECT_TOKEN_IN_RESPONSE, setCachingEnabled } from '../interceptors/token.interceptor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private teacherSignal = signal<TeacherProfile | null>(null);
+  private studentSignal = signal<StudentProfile | null>(null);
+
+  teacherProfile$ = this.teacherSignal.asReadonly();
+  studentProfile$ = this.studentSignal.asReadonly();
 
   private BASE_URL = enviroment.BASE_URL;
 
@@ -45,15 +50,14 @@ export class AuthService {
     )
   }
   register(userRegister:UserRegister){
-    return this.http.post(`${this.BASE_URL}/auth/login`,userRegister)
+    return this.http.post(`${this.BASE_URL}/auth/register`,userRegister)
     .pipe(
       catchError((error:HttpErrorResponse)=>{
         if(error.status === HttpStatusCode.Conflict){
           return throwError(()=> new Error('Ya se registro el email'))
         }
         return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde'))
-      }
-      )
+      })
     )
   }
   registerAndLogin(userRegister:UserRegister){
@@ -64,7 +68,32 @@ export class AuthService {
   }
   logout(){
     this.tokenService.removeToken();
+    this.teacherSignal.set(null);
+    this.studentSignal.set(null);
     this.router.navigate(['/auth/login'])
   }
+  getProfile(){
+    const token = this.tokenService.getToken();
+    if(!token) return throwError(()=> new Error('Rol de usuario no reconocido'));
+    const role = this.tokenService.getUserRole();
+    if(role==='ESTUDIANTE')
+      return this.http.get<StudentProfile>(`${this.BASE_URL}/estudiantes/profile`,{context: setCachingEnabled()})
+      .pipe(
+        tap(profile=> this.studentSignal.set(profile)),
+        this.handleProfileError
+      );
+    else if(role==='PROFESOR')
+      return this.http.get<TeacherProfile>(`${this.BASE_URL}/profesores/profile`,{context: setCachingEnabled()})
+      .pipe(
+        tap(profile=> this.teacherSignal.set(profile)),
+        this.handleProfileError
+      );
+    else
+      return throwError(()=> new Error('Rol de usuario no reconocido'));
+  }
+
+  private handleProfileError = catchError((error: HttpErrorResponse) => {
+    return throwError(() => new Error('Ups algo salió mal, inténtelo más tarde'));
+  });
 
 }
