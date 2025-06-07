a6bc47d38984bfbd0b7d1df1ff1b8437fc01f0c6
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:21:12 -0500

    Add authGuard to teacher routes for route protection

--- src/app/teacher/teacher.routes.ts
+++ src/app/teacher/teacher.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from "@angular/router";
 import { LayoutComponent } from "./pages/layout/layout.component";
+import { roleGuard } from "../core/guards/role.guard";
+import { authGuard } from "../core/guards/auth.guard";
 
 export const TEACHER_ROUTES: Routes = [
 
@@ -9,22 +11,27 @@ export const TEACHER_ROUTES: Routes = [
         children: [
             {
                 path: 'home',
+                canActivate: [authGuard],
                 loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent)
             },
             {
                 path: 'dashboard',
+                canActivate: [authGuard],
                 loadComponent: () => import("./pages/dashboard/dashboard.component").then(m => m.DashboardComponent)
             },
             {
                 path: 'sesiones',
+                canActivate: [authGuard],
                 loadComponent: () => import("./pages/sessions/sessions.component").then(m => m.SessionsComponent)
             },
             {
                 path: 'cursos',
+                canActivate: [authGuard],
                 loadComponent: () => import("./pages/courses/courses.component").then(m => m.CoursesComponent)
             },
             {
                 path: 'cuenta',
+                canActivate: [authGuard],
                 loadComponent: () => import("./pages/account/account.component").then(m => m.AccountComponent)
             },
             {
