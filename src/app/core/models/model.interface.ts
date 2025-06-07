cb036c5da806acef9dda9edff2e9c93de57446f8
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:22:26 -0500

    Add user profile and course interfaces for improved type safety

--- src/app/core/models/model.interface.ts
+++ src/app/core/models/model.interface.ts
@@ -8,3 +8,24 @@ export interface UserRegister{
     paternalLastname:string;
     gender:Gender;
 }
+export interface Course{
+    courseId:string;
+    name:string;
+    teacherId:string;
+}
+export interface UserProfile{
+    email:string;
+    name:string;
+    maternalLastname:string;
+    paternalLastname:string;
+    gender:Gender;
+    rol:Role;
+
+}
+export interface TeacherProfile extends UserProfile {
+    teacherId:string;
+    courses:Course[];
+}
+export interface StudentProfile extends UserProfile{
+    studentId:string;
+}
