efe368be160aaa64b2cebbb664112c52bede21a4
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:22:10 -0500

    Refactor layout component to use grid layout and integrate TeacherNavbar; add auth service for profile management

--- src/app/teacher/pages/layout/layout.component.ts
+++ src/app/teacher/pages/layout/layout.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TeacherNavbarComponent } from '../../components/navbar/navbar.component';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TeacherNavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  private authServ = inject(AuthService);
+  constructor(){
+    this.authServ.getProfile().subscribe();
+  }
+  ngOnDestroy(): void {
+    this.authServ.logout();
+  }
 
 }
