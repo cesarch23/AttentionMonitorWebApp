0f6696cece8b44a6d3a531333f2267f6c06fa067
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:21:58 -0500

    Add authGuard to student routes for route protection

--- src/app/student/student.routes.ts
+++ src/app/student/student.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { authGuard } from "../core/guards/auth.guard";
 
 export const STUDENT_ROUTES: Routes = [
     {
         path: 'home',
+        canActivate: [authGuard],
         component: HomeComponent
     },
     {
         path: 'cuenta',
+        canActivate: [authGuard],
         loadComponent: () => import("./pages/account/account.component").then(m => m.AccountComponent)
     },
     {
