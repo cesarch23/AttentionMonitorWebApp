314328b851e90770356ecc206c3ad8aea1573de2
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 00:21:37 -0500

    Implement responsive navbar with toggle functionality and profile menu

--- src/app/teacher/components/navbar/navbar.component.ts
+++ src/app/teacher/components/navbar/navbar.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { NgClass } from '@angular/common';
+
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'teacher-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatIconModule,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatMenuModule,
+    NgClass
+  ],
+  //exportAs:[TeacherNavbarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class TeacherNavbarComponent {
+  private authServ = inject(AuthService);
+  teacher = this.authServ.teacherProfile$;
 
+  showMenu = false;
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
+  logout() {
+    this.authServ.logout();
+  }
 }
