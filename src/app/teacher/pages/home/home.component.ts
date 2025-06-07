7e542fce2825d939d05140c2b0aad9c722e2d9e4
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 13:14:56 -0500

    Implement home component layout with dynamic course display and styling

--- src/app/teacher/pages/home/home.component.ts
+++ src/app/teacher/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  private authServ = inject(AuthService);
+  teacher$ = this.authServ.teacherProfile$;
+
+  ngOnInit(): void {
+    
+  }
+  
+  
+
+
 
 }
