017778809fcc28540cfe2622f2dc8b2dc37d9ee6
Cesar Choccata <c21choccata@gmail.com>
2025-06-07 13:14:42 -0500

    Refactor teacher routes to use component references instead of lazy loading for improved readability

--- src/app/teacher/teacher.routes.ts
+++ src/app/teacher/teacher.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from "@angular/router";
 import { LayoutComponent } from "./pages/layout/layout.component";
 import { roleGuard } from "../core/guards/role.guard";
 import { authGuard } from "../core/guards/auth.guard";
+import { HomeComponent } from "./pages/home/home.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { SessionsComponent } from "./pages/sessions/sessions.component";
+import { CoursesComponent } from "./pages/courses/courses.component";
 
 export const TEACHER_ROUTES: Routes = [
 
@@ -12,22 +16,29 @@ export const TEACHER_ROUTES: Routes = [
             {
                 path: 'home',
                 canActivate: [authGuard],
-                loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent)
+                component: HomeComponent
             },
             {
                 path: 'dashboard',
                 canActivate: [authGuard],
-                loadComponent: () => import("./pages/dashboard/dashboard.component").then(m => m.DashboardComponent)
+                component: DashboardComponent
             },
             {
                 path: 'sesiones',
                 canActivate: [authGuard],
-                loadComponent: () => import("./pages/sessions/sessions.component").then(m => m.SessionsComponent)
+                component: SessionsComponent
             },
+            {
+                path: 'sesiones/:courseId',
+                canActivate: [authGuard],
+                loadComponent: () => import("./pages/sessions-by-course/sessions-by-course.component").then(m => m.SessionsByCourseComponent)
+            },
+
             {
                 path: 'cursos',
                 canActivate: [authGuard],
-                loadComponent: () => import("./pages/courses/courses.component").then(m => m.CoursesComponent)
+                component: CoursesComponent,
+                
             },
             {
                 path: 'cuenta',
