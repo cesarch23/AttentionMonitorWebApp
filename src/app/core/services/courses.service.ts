8e299767dbd32962623f7dd88ff7f723828decbe
Cesar Choccata <c21choccata@gmail.com>
2025-06-09 01:34:49 -0500

    Refactor CourseService to improve error handling and add reactive courses signal

--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -1,21 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { AuthService } from './auth.service';
 import { enviroment } from '../../../environments/environment';
 import { setCachingEnabled } from '../interceptors/token.interceptor';
+import { Course } from '../models/model.interface';
+import { catchError, retry, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CoursesService {
+export class CourseService {
   private http = inject(HttpClient);
   private authService = inject(AuthService);
   private url = `${enviroment.BASE_URL}/cursos`;
+
+  private coursesSignal = signal<Course[]>([]);
+  courses$ = this.coursesSignal.asReadonly()
 
   constructor() { }
 
    getCoursesByTeacherId() {
     const teacherId = this.authService.teacherProfile$()?.teacherId;
-    return this.http.get(`${this.url}/${teacherId}`, { context: setCachingEnabled() });
+    console.log("geacher id",teacherId)
+    return this.http.get<Course[]>(`${this.url}/${teacherId}`, { context: setCachingEnabled() })
+    .pipe(
+      retry(1),
+      tap((courses)=>{
+        this.coursesSignal.update(()=>[...courses])
+        console.log(" curssos service ",courses)
+      }),
+      catchError((error:HttpErrorResponse)=>{
+        if(error.status === HttpStatusCode.Unauthorized){
+            return throwError(()=> new Error('Credenciales inválidas. Verifica tu correo y contraseña'))
+          }
+          return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde'))
+        }
+      )
+      
+    );
   }
 }
