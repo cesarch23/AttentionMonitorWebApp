a6582f0fa2c1ff516ec268a809eb712c5ab79dab
Cesar Choccata <c21choccata@gmail.com>
2025-06-09 01:33:26 -0500

    Add date formatting configuration to appConfig for improved date handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,29 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+ 
 
+const CUSTOM_FORMAT = {
+  parse: {
+    dateInput: 'dd MMM yyyy',
+  },
+  display: {
+    dateInput: 'dd MMM yyyy',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'dd MMM yyyy',
+    monthYearA11yLabel: 'MMMM yyyy',
+  },
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([tokenInterceptor])
-    )
+    ),
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_FORMATS, useValue:CUSTOM_FORMAT },
+    { provide: MAT_DATE_LOCALE,useValue:'es-PE' },
   ]
 };
