7718544c2cdae6d681fc2d5679c4c1e47cb0b4a9
Cesar Choccata <c21choccata@gmail.com>
2025-06-09 19:33:43 -0500

    Refactor session retrieval logic to improve error handling and update session state management

--- src/app/core/services/sessions.service.ts
+++ src/app/core/services/sessions.service.ts
@@ -2,9 +2,11 @@ import { inject, Injectable, signal } from '@angular/core';
 import { AuthService } from './auth.service';
 import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { enviroment } from '../../../environments/environment';
-import { Session } from '../models/model.interface';
+import { Session, SessionRegister, TeacherProfile } from '../models/model.interface';
 import { setCachingEnabled } from '../interceptors/token.interceptor';
-import { catchError, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, retry, switchMap, tap, throwError } from 'rxjs';
+import { getDurationInMinutes } from '../../shared/utils/time-range.validator';
+import { DateTime } from 'luxon';
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +21,49 @@ export class SessionsService {
 
   constructor() { }
   getSessionsByTeacherId() {
-    const teacherId = this.authService.teacherProfile$()!.teacherId || '0'; 
+    const teacherId = this.authService.teacherProfile$()?.teacherId;
+    if(!teacherId){
+      return this.http.get<TeacherProfile>(`${enviroment.BASE_URL}/profesores/profile`,{context: setCachingEnabled()})
+      .pipe(
+        switchMap(profile=>{
+          return this.http.get<Session[]>(`${this.url}/profesor/${profile.teacherId}`, { context: setCachingEnabled() })
+            .pipe(
+              tap((sessions: Session[]) => {
+                this.sessionsSignal.update(()=>sessions); 
+              }),
+              catchError((error:HttpErrorResponse)=>{
+                if(error.status === HttpStatusCode.Unauthorized){
+                  return throwError(()=> new Error('Credenciales inválidas. vuelve a iniciar sesión'))
+                }
+                return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde'))
+              })
+            );
+        })
+      );
+    }
     return this.http.get<Session[]>(`${this.url}/profesor/${teacherId}`, { context: setCachingEnabled() })
-    .pipe(
-      tap((sessions: Session[]) => {
-        this.sessionsSignal.update(()=>sessions); 
+            .pipe(
+              tap((sessions: Session[]) => {
+                this.sessionsSignal.update(()=>sessions); 
+              }),
+              catchError((error:HttpErrorResponse)=>{
+                if(error.status === HttpStatusCode.Unauthorized){
+                  return throwError(()=> new Error('Credenciales inválidas. vuelve a iniciar sesión'))
+                }
+                return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde'))
+              })
+            );
+    
+  }
+  add(session:SessionRegister){
+    const sessionDuration = getDurationInMinutes(session.startHours, session.endHours)
+    const date = DateTime.fromJSDate(session.date).toFormat('yyyy-MM-dd');
+    console.log("sesion para agregar",session,date)
+     return this.http.post<Session>(`${this.url}`,{...session,sessionDuration,date},{ context:setCachingEnabled() }).pipe(
+      tap((session: Session) => {
+        this.sessionsSignal.update((current)=> [...current,session]); 
       }),
-      catchError((error:HttpErrorResponse)=>{
+      catchError((error)=>{
         if(error.status === HttpStatusCode.Unauthorized){
           return throwError(()=> new Error('Credenciales inválidas. vuelve a iniciar sesión'))
         }
