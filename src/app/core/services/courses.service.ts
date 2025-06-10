809601dfb9117e3bd815e584bf4308a5b74851d6
Cesar Choccata <c21choccata@gmail.com>
2025-06-10 01:55:07 -0500

    Refactor CourseService to enhance error handling and add course management methods

--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { enviroment } from '../../../environments/environment';
 import { setCachingEnabled } from '../interceptors/token.interceptor';
 import { Course } from '../models/model.interface';
-import { catchError, retry, switchMap, tap, throwError } from 'rxjs';
+import { catchError, Observable, retry, switchMap, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +29,7 @@ export class CourseService {
     }
     return this.sendRequest(teacherId);
     
-  }
+   }
   private sendRequest(teacherId:string){
     return this.http.get<Course[]>(`${this.url}/${teacherId}`, { context: setCachingEnabled() })
     .pipe(
@@ -48,4 +48,58 @@ export class CourseService {
       
     );
   }
+  addCourse(course:{name:string} ): Observable<Course>{
+    const { name } = course;
+    const teacherId = this.authService.teacherProfile$()!.teacherId;
+    if(!teacherId){
+      return this.authService.getTeacherProfile().pipe(
+        switchMap((teacher)=>  this.sendRequestToAdd({name , teacherId: teacher.teacherId } ) ) 
+      )
+    }
+    return this.sendRequestToAdd({name,teacherId});
+  }
+  updateCourse(name:string, courseId:string ): Observable<Course>{
+    return this.http.put<Course>(`${this.url}/${courseId}`,{name},{context:setCachingEnabled()}).pipe(
+      retry(1),
+      tap(course=>this.coursesSignal.update((courses)=>[...courses,course])),
+      catchError((error:HttpErrorResponse)=>{
+        if(error.status === HttpStatusCode.Unauthorized){
+            return throwError(()=> new Error('Credenciales inválidas. Verifica tu correo y contraseña'))
+          }
+          return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde')) 
+        }
+      )
+    );
+  }
+  deleteCourse(courseId:string){
+    return this.http.delete<Boolean>(`${this.url}/${courseId}`,{context:setCachingEnabled()}).pipe(
+      retry(1),
+      tap(course=>this.coursesSignal.update((courses)=>[...courses.filter(c => c.courseId !== courseId)])),
+      catchError((error:HttpErrorResponse)=>{
+        if(error.status === HttpStatusCode.Unauthorized){
+            return throwError(()=> new Error('Credenciales inválidas. Verifica tu correo y contraseña'))
+          }
+        if(error.status === HttpStatusCode.Conflict){
+            return throwError(()=> new Error('No se pudo eliminar,el curso tiene sesiones asociadas'))
+          }
+          return throwError(()=> new Error('Ups algo salio mal. No se pudo eliminar,intentelo más tarde')) 
+        }
+      )
+    );
+  }
+
+  private sendRequestToAdd(courseSend: {teacherId:string,name:string}) : Observable<Course>{
+    return this.http.post<Course>(`${this.url}`,courseSend,{context:setCachingEnabled()}).pipe(
+      retry(1),
+      tap(course=>this.coursesSignal.update((courses)=>[...courses,course])),
+      catchError((error:HttpErrorResponse)=>{
+        if(error.status === HttpStatusCode.Unauthorized){
+            return throwError(()=> new Error('Credenciales inválidas. Verifica tu correo y contraseña'))
+          }
+          return throwError(()=> new Error('Ups algo salio mal, intentelo más tarde')) 
+        }
+      )
+    );
+  }
+
 }
