243ef683210734927e077fdba55601aceec2e83f
Cesar Choccata <c21choccata@gmail.com>
2025-06-10 18:08:55 -0500

    Add dashboard component layout and integrate chart components

--- src/app/teacher/pages/dashboard/dashboard.component.ts
+++ src/app/teacher/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MultitaskingChartComponent } from "../../components/multitasking-chart/multitasking-chart.component";
+import { SomnolenceChartComponent } from "../../components/somnolence-chart/somnolence-chart.component";
+import { AbsenceChartComponent } from "../../components/absence-chart/absence-chart.component";
+import { AttentionChartComponent } from "../../components/attention-chart/attention-chart.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatIcon,
+    MatButton,
+    MatFormFieldModule,
+    MultitaskingChartComponent,
+    SomnolenceChartComponent,
+    AbsenceChartComponent,
+    AttentionChartComponent
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+    nivelesAtencion=[80, 85, 75, 70, 60, 65, 68, 70, 75, 78, 80, 82, 85];
 }
