a02b773fab86bd7c68bba0bbc05ffd8eb0ae261f
Cesar Choccata <c21choccata@gmail.com>
2025-06-09 19:34:53 -0500

    Implement sessions component with loading state, error handling, and session retrieval logic

--- src/app/teacher/pages/sessions/sessions.component.ts
+++ src/app/teacher/pages/sessions/sessions.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SessionsService } from '../../../core/services/sessions.service';
+import { RequestStatus, Session } from '../../../core/models/model.interface';
+import { NotificationService } from '../../../core/services/notification.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SessionsTableComponent } from "../../components/sessions-table/sessions-table.component";
+import { SessionDialogComponent } from '../../components/session-dialog/session-dialog.component';
 
 @Component({
   selector: 'app-sessions',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    SessionsTableComponent,
+],
   templateUrl: './sessions.component.html',
   styleUrl: './sessions.component.css'
 })
-export class SessionsComponent {
+export class SessionsComponent implements OnInit {
+  sessionStatus:RequestStatus = 'init';
+  private sessionServ = inject(SessionsService)
+  private notification = inject(NotificationService)
+  private dialog = inject(MatDialog);
+  sessions:Session[] = []
+  
+  constructor(){
+    console.log("iniciado la session componente")
+  }
+  
+  ngOnInit(): void {
+     this.sessionStatus = 'loading';
+    this.sessionServ.getSessionsByTeacherId().subscribe({
+          next: (sessions) => {
+            this.sessionStatus = 'success';
+            this.sessions = structuredClone(sessions)
+          },
+          error: (messageError) => {
+            this.sessionStatus = 'failed';
+            this.notification.show(messageError,'error');
+          }
+    })
+  }
+
+   openToAddSessionDialog(){
+      this.dialog.open(SessionDialogComponent,{
+        data:{
+          title:'Agregar sesión',
+          isEdit:false
+        }
+      }).afterClosed().subscribe((result: { session?:Session;updated:boolean; inserted:boolean}={inserted:false,updated:false})=>{
+        //insertar 
+        if(result.inserted && result.session){
+          const pastSession= structuredClone(this.sessions)
+          this.sessions = [result.session,...pastSession]
+        }
+        console.log("results recibidos",result)
+        //actualizar
+      })
+    }
 
 }
