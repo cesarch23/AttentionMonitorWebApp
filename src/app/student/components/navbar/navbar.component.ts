8362a4548e4ec31d4323c02eca98a942d11c3e57
Cesar Choccata <c21choccata@gmail.com>
2025-06-10 23:24:33 -0500

    Implement navbar component with responsive design and authentication integration

--- src/app/student/components/navbar/navbar.component.ts
+++ src/app/student/components/navbar/navbar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'student-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatIconModule,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+   private authServ = inject(AuthService);
+   student = this.authServ.studentProfile$;
+  
+    showMenu = false;
+    toggleNavbar(){
+      this.showMenu = !this.showMenu;
+    }
+    logout() {
+      this.authServ.logout();
+    }
 }
