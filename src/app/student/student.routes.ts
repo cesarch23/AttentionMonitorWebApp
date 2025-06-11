fbcf0bdf0156657a5c40ed8913ecae4669c01191
Cesar Choccata <c21choccata@gmail.com>
2025-06-10 23:24:20 -0500

    Refactor student routes to use layout component and nested routing

--- src/app/student/student.routes.ts
+++ src/app/student/student.routes.ts
@@ -1,21 +1,29 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { authGuard } from "../core/guards/auth.guard";
+import { LayoutComponent } from "./pages/layout/layout.component";
 
 export const STUDENT_ROUTES: Routes = [
     {
-        path: 'home',
-        canActivate: [authGuard],
-        component: HomeComponent
-    },
-    {
-        path: 'cuenta',
-        canActivate: [authGuard],
-        loadComponent: () => import("./pages/account/account.component").then(m => m.AccountComponent)
-    },
-    {
-        path: '**',
-        redirectTo: 'home' // Redirect any unknown paths to home
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'home',
+                canActivate: [authGuard],
+                loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent)
+            },
+             {
+                path: 'cuenta',
+                canActivate: [authGuard],
+                loadComponent: () => import("./pages/account/account.component").then(m => m.AccountComponent)
+            },
+            {
+                path: '**',
+                redirectTo: 'home' // Redirect any unknown paths to home
+            }
+        ]
     }
+   
 
 ];
