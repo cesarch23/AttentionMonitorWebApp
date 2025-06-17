d87252693e101944d9d1da1b3e5e89c5158126c6
Cesar Choccata <c21choccata@gmail.com>
2025-06-17 10:51:59 -0500

    Implement showMultitaskingStudents method and update button action in MultitaskingChartComponent

--- src/app/teacher/components/multitasking-chart/multitasking-chart.component.ts
+++ src/app/teacher/components/multitasking-chart/multitasking-chart.component.ts
@@ -1,11 +1,13 @@
-import { Component, computed, effect, input, signal } from '@angular/core';
+import { Component, computed, effect, Input, input, signal } from '@angular/core';
 
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { PhoneLineChart } from '../../../core/models/model.interface';
+import { PhoneLineChart, Session } from '../../../core/models/model.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { MultitaskingTableComponent } from '../multitasking-table/multitasking-table.component';
 
 @Component({
   selector: 'teacher-multitasking-chart',
@@ -22,13 +24,13 @@ import { PhoneLineChart } from '../../../core/models/model.interface';
 export class MultitaskingChartComponent {
 
   phoneUseData = input<PhoneLineChart | null>(null);
-
+  @Input({ required: true }) session= signal<Session | null> (null);
   lineChartData = signal<ChartConfiguration<'line'>['data']>({
     labels: [],
     datasets: [],
   });
 
-  constructor() {
+  constructor(private dialog:MatDialog) {
     effect(() => {
     const data = this.phoneUseData();
     if (!data) return;
@@ -79,7 +81,14 @@ export class MultitaskingChartComponent {
       },
     },
   };
-
+  showMultitaskingStudents(){
+      if(!this.session()?.sessionId) return;
+      this.dialog.open(MultitaskingTableComponent,{ 
+        data: { sessionId: this.session()!.sessionId },
+        minWidth:'400px',
+        maxWidth: '900px'
+      })
+    }
    
 
 }
