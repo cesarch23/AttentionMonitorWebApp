b85432531b79be6cb012e95c076ba8b58ef346e5
Cesar Choccata <c21choccata@gmail.com>
2025-07-01 19:46:54 -0500

    Add recover route to authentication routes

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from "@angular/router";
 export const AUTH_ROUTES: Routes = [
     { path: 'register', loadComponent: () => import("./register/register.component").then(m => m.RegisterComponent) },
     { path: 'login', loadComponent: () => import("./login/login.component").then(m => m.LoginComponent) },
+    { path: 'recover', loadComponent: () => import("./recover/recover.component").then(m => m.RecoverComponent) },
     { path: '**', redirectTo: 'login' } // Redirect any unknown paths to login
 ];
