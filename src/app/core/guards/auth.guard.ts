d18d3f48d2b87310bc5742d6d6e1817117c3274b
Cesar Choccata <c21choccata@gmail.com>
2025-10-29 22:10:10 -0500

    add notification for session timeout in auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,12 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '../services/token.service';
+import { NotificationService } from '../services/notification.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const tokenService = inject(TokenService);
+  const notificationService = inject(NotificationService);
   const router = inject(Router);
   if(tokenService.isValidToken()) return true;
   else {
+    notificationService.show("La sesión se cerró automáticamente por inactividad. Vuelva a iniciar sesión", 'warning');
     router.navigate(['/auth/login'])
     return false;
   }
