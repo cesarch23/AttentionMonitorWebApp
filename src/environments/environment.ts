c71a6bf4dfe49a1ffa9ff6106447d985da843c6d
Cesar Choccata <c21choccata@gmail.com>
2025-10-29 23:01:27 -0500

    update environment configuration and refine .gitignore for local files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production:false,
-    BASE_URL:""
+    // BASE_URL:"http://localhost:8082/api/v1"
+    BASE_URL:"https://backend.agreeabledesert-74144f7c.brazilsouth.azurecontainerapps.io/api/v1"
     
 }
