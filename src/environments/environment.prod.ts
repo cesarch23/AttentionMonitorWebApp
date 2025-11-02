61efee992d8b2f88d56396af47f2da7bc549532b
Cesar Choccata <c21choccata@gmail.com>
2025-11-02 15:28:50 -0500

    update base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    BASE_URL:"https://back-2-br.agreeabledesert-74144f7c.brazilsouth.azurecontainerapps.io/api/v1"
+    BASE_URL:"https://backend.agreeabledesert-74144f7c.brazilsouth.azurecontainerapps.io/api/v1"
 }
